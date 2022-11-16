52279772a537fdcba54bd39617f35be288d293b6
hmisty <hmisty@gmail.com>
2022-11-17 00:53:00 +0800

    block list async; status check

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -1,22 +1,70 @@
 angular.module('ethExplorer')
-    .controller('mainCtrl', function ($rootScope, $scope, $location) {
+	.controller('mainCtrl', function ($rootScope, $scope, $location, $routeParams, $q) {
 
-	var web3 = $rootScope.web3;
-	var maxBlocks = 15; // TODO: into setting file or user select
-		// XXX reduced from 50 to 15 to speed up the page loading.
-		
-	var blockNum = $scope.blockNum = parseInt(web3.eth.blockNumber, 10);
-	if (maxBlocks > blockNum) {
-	    maxBlocks = blockNum + 1;
-	}
+		var web3 = $rootScope.web3;
 
-	// get latest N blocks
-	$scope.blocks = [];
-	for (var i = 0; i < maxBlocks; ++i) {
-		var blockInfo = web3.eth.getBlock(blockNum - i);
-		blockInfo.time_localestring = new Date(blockInfo.timestamp * 1000).toLocaleString('zh-CN', { timezone: 'UTC', timeZoneName: 'short' });
+		$scope.init = function () {
+			var maxBlocks = 5; // TODO: into setting file or user select
+			// XXX reduced from 50 to 15 to speed up the page loading.
 
-	    $scope.blocks.push(blockInfo);
-	}
+			getBlockHeight().then(function (result) {
+				var blockNum = $scope.blockNum = result;
+				if (maxBlocks > blockNum) {
+					maxBlocks = blockNum + 1;
+				}
 
-    });
+				// get latest N blocks
+				$scope.blocks = [];
+				for (var i = 0; i < maxBlocks; ++i) {
+					getBlockInfo(blockNum - i).then(function (result) {
+						if (result.number == blockNum) {
+							var current = Date.now() / 1000;
+							console.log("local time: ", current, " block time: ", result.timestamp);
+							if (current - result.timestamp < 60) { // last block is within 60 sec
+								$scope.status = "🟢 正常";
+							} else {
+								$scope.status = "🔴 异常";
+							}
+						}
+
+						//$scope.blocks.push(result);
+						var j = 0;
+						while (j < $scope.blocks.length && result.number < $scope.blocks[j].number) {
+							j++;
+						}
+						$scope.blocks.splice(j, 0, result);
+					});
+				}
+			});
+		}
+
+		function getBlockHeight() {
+			var deferred = $q.defer();
+
+			var height = parseInt(web3.eth.blockNumber, 10);
+
+			// intentionally delay...
+			window.setTimeout(function() {
+				deferred.resolve(height);
+			}, 0 /*Math.floor(Math.random() * 3000) + 2000*/);
+
+			return deferred.promise;
+		}
+
+		function getBlockInfo(blockNum) {
+			var deferred = $q.defer();
+
+			var blockInfo = web3.eth.getBlock(blockNum);
+			blockInfo.time_localestring = new Date(blockInfo.timestamp * 1000).toLocaleString('zh-CN', { timezone: 'UTC', timeZoneName: 'short' });
+
+			// intentionally delay...
+			window.setTimeout(function() {
+				deferred.resolve(blockInfo);
+			}, 0 /*Math.floor(Math.random() * 3000) + 2000*/);
+
+			return deferred.promise;
+		}
+
+		$scope.init();
+
+	});
